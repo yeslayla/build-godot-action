4f2170064dc78cb5108ea25121768799f786249b
Joseph Manley <j@cloudsumu.com>
2019-11-29 15:53:20 -0500

    Basic action
    Opened permissions
    
    Move setup to Dockerfile
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM barichello/godot-ci:latest
+
+LABEL "com.github.actions.name"="Build Godot"
+LABEL "com.github.actions.description"="Build a Godot project for multiple platforms"
+LABEL "com.github.actions.icon"="loader"
+LABEL "com.github.actions.color"="blue"
+
+LABEL version="0.0.0"
+LABEL repository="https://github.com/josephbmanley/build-godot-action"
+LABEL homepage="https://cloudsumu.com/"
+LABEL maintainer="Joseph Manley <joseph@cloudsumu.com>"
+
+USER root
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+wget https://downloads.tuxfamily.org/godotengine/3.1.1/Godot_v3.1.1-stable_export_templates.tpz --quiet
+mkdir ~/.cache
+mkdir -p ~/.config/godot
+mkdir -p ~/.local/share/godot/templates/3.1.1.stable
+unzip Godot_v3.1.1-stable_export_templates.tpz
+mv templates/* ~/.local/share/godot/templates/3.1.1.stable
+rm -f Godot_v3.1.1-stable_export_templates.tpz
+
+# Export for Linux
+mkdir -p ./build/linux
+godot --export Linux/X11 ./build/linux/${PROJECT}
+
+# Export for Windows
+mkdir -p ./build/windows
+godot --export "Windows Desktop" ./build/windows/${PROJECT}.exe
+
+# Export for OSX
+mkdir -p ./builds/mac
+godot --export "Mac OSX" ./build/mac/${PROJECT}
