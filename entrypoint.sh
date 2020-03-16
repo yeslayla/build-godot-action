e8c3cda194b19a0448e0432a51b35e36f3e2f59c
Joseph Manley <j@cloudsumu.com>
2020-03-16 18:21:33 -0400

    BUGFIX: Update Godot version
    BUGFIX: Update Godot version

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Install export templates
-wget https://downloads.tuxfamily.org/godotengine/3.2/Godot_v3.2-stable_export_templates.tpz --quiet
+wget https://downloads.tuxfamily.org/godotengine/3.2.1/Godot_v3.2.1-stable_export_templates.tpz --quiet
 mkdir ~/.cache
 mkdir -p ~/.config/godot
-mkdir -p ~/.local/share/godot/templates/3.2.stable
-unzip Godot_v3.2-stable_export_templates.tpz
-mv templates/* ~/.local/share/godot/templates/3.2.stable
-rm -f Godot_v3.2-stable_export_templates.tpz
+mkdir -p ~/.local/share/godot/templates/3.2.1.stable
+unzip Godot_v3.2.1-stable_export_templates.tpz
+mv templates/* ~/.local/share/godot/templates/3.2.1.stable
+rm -f Godot_v3.2.1-stable_export_templates.tpz
 
 if [ "$3" != "" ]
 then
