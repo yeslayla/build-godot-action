7f89f866c877ccd5dc5e1cf0e898bec1954e7e5b
stonedDiscord <Tukz@gmx.de>
2023-05-06 23:35:27 +0200

    Allow Spaces in `projectDir` (#21)
    * try to fix the space issue
    
    * change project dir
    
    * spaced preset name

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,8 @@ fi
 # Export for project
 echo "Building $1 for $2"
 mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
-cd ${5-"$GITHUB_WORKSPACE"}
-godot --${mode} $2 $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
+cd "$GITHUB_WORKSPACE/$5"
+godot --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
 echo "Build Done"
 
 echo ::set-output name=build::build/${SubDirectoryLocation:-""}
